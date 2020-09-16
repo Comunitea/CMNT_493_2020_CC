862ec9e86d46424fe6ca26e556161cb5d8185f0b
javierjcf <javierjcf@gmail.com>
2020-09-16 13:51:00 +0200

    [FIX]product_serial_number: Campos custom en primer plano

--- project-addons/product_serial_number/models/product.py
+++ project-addons/product_serial_number/models/product.py
@@ -9,7 +9,18 @@ class ProductTemoplate(models.Model):
 
     serial_mgmt = fields.Boolean('Manage by serial number', default=True)
     auto_create_lot = fields.Boolean(default=True)
-    tracking = fields.Selection(default='serial')
+    # tracking = fields.Selection(default='serial')
+
+    @api.model
+    def default_get(self, default_fields):
+        """
+        Get the payment fields filled when opening from purchase order
+        """
+        res = super().default_get(default_fields)
+        res.update({
+            'tracking': 'serial',
+        })
+        return res
 
     @api.onchange('serial_mgmt')
     def _onchange_serial_mgmt(self):
