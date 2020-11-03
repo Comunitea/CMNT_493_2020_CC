20308ed49395bb519bc809efea52d01dddf326a1
javierjcf <javoerjcf@gmail.com>
2020-11-03 11:49:15 +0100

    [

---
+++ scripts/celan_db.sql
@@ -0,0 +1,13 @@
+
+delete from sale_order;
+delete from stock_picking;
+delete from stock_move
+delete from stock_move_line;
+delete from stock_quant
+delete from purchase_attribute_line;
+delete from purchase_order_line;
+delete from purchase_order;
+delete from stock_production_lot
+delete from account_move_line;
+delete from account_move;
+delete from account_payment;
